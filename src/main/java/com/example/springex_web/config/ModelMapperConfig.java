015cd5857a579e02a6d3e04d0fc524141a60f290
ynkite <tkddus3521@naver.com>
2026-03-25 21:18:32 +0900

    springex 프로젝트에서 controller 폴더 가져옴 dto 폴더 가져옴 mapper 폴더 가져옴
    webapp / web-INF안에
    views 폴더 가져옴
    lombok.config 가져옴
    
    ex.jsp예제들은 지움
    
    WEB-INF에 root-context.xml, servlet-context.xml가져옴
    
    main안에 mapper폴더와 log4j2.xml을 가져옴
    
    web.xml 가져옴

---
+++ src/main/java/com/example/springex_web/config/ModelMapperConfig.java
@@ -0,0 +1,20 @@
+package com.example.springex_web.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+// bean 설정에 대한 클래스다라는 것을 보여줌
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper getMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper;
+    }
+}
